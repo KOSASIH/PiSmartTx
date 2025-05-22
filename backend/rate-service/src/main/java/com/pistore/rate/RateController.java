014c779f4992209806f88258f89f7868bbe78994
KOSASIH <kosasihg88@gmail.com>
2025-05-22 15:27:53 +0700

    Update RateController.java

--- backend/rate-service/src/main/java/com/pistore/rate/RateController.java
+++ backend/rate-service/src/main/java/com/pistore/rate/RateController.java
@@ -4,6 +4,7 @@ package com.pistore.rate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class RateController {
     private RateService rateService;
 
     @GetMapping("/pi")
-    public RateDTO getPiRate() {
-        return rateService.getPiRate();
+    public RateDTO getPiRate(@RequestParam String type) {
+        return rateService.getPiRate(type);
     }
 }
