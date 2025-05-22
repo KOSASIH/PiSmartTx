d39629d361837b36a0e05cb1f8c8dd2041d40a16
KOSASIH <kosasihg88@gmail.com>
2025-05-22 14:05:42 +0700

    Create RateController.java

---
+++ backend/rate-service/src/main/java/com/pistore/rate/RateController.java
@@ -0,0 +1,19 @@
+// backend/rate-service/src/main/java/com/pistore/rate/RateController.java
+package com.pistore.rate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rate")
+public class RateController {
+    @Autowired
+    private RateService rateService;
+
+    @GetMapping("/pi")
+    public RateDTO getPiRate() {
+        return rateService.getPiRate();
+    }
+}
