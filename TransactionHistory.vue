f6db145a5f5c66831d1822ed91a0b34a3bf68d80
KOSASIH <kosasihg88@gmail.com>
2025-05-25 13:56:38 +0700

    Create TransactionHistory.vue

---
+++ TransactionHistory.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Transaction History</h2>
+        <table>
+            <tr v-for="transaction in transactions" :key="transaction.id">
+                <td>{{ transaction.date }}</td>
+                <td>{{ transaction.amount }}</td>
+                <td>{{ transaction.status }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            transactions: []
+        };
+    },
+    mounted() {
+        this.fetchTransactions();
+    },
+    methods: {
+        fetchTransactions() {
+            // Fetch transaction data from the backend
+        }
+    }
+};
+</script>
