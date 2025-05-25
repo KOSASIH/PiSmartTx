83dc586f59d1994d306cd972cc1d09b59574a0ac
KOSASIH <kosasihg88@gmail.com>
2025-05-25 14:03:16 +0700

    Create AnalyticsDashboard.vue

---
+++ AnalyticsDashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Blockchain Analytics Dashboard</h2>
+        <div>
+            <p>Total Transactions: {{ totalTransactions }}</p>
+            <p>Active Users: {{ activeUsers }}</p>
+            <p>Network Performance: {{ networkPerformance }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            totalTransactions: 0,
+            activeUsers: 0,
+            networkPerformance: 'Good'
+        };
+    },
+    mounted() {
+        this.fetchAnalytics();
+    },
+    methods: {
+        fetchAnalytics() {
+            // Fetch analytics data from the backend
+        }
+    }
+};
+</script>
